ccbff3b97436b4e8bbc1ee679df8ef16427dd2ba
kekzdealer <kekzdealer@gmail.com>
2023-01-25 10:44:14 +0100

    discord functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+use serenity::async_trait;
+// for global commands
+//use serenity::model::application::command::Command;
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                // More commands go here
+                _ => "not implemented :(".to_string(),
+            };
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+        let guild_id = GuildId(
+                std::env::var("GUILD_ID")
+                .expect("GUILD_ID must be set.")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| commands::ping::register(command))
+                // More commands go here
+        })
+        .await;
+        println!("I now have the following guild slash commands: {:#?}", commands);
+        // let guild_command = Command::create_global_application_command(&ctx.http, |command| {
+        //    commands::ping::register(command)
+        //})
+        //.await;
+        //println!("I created the following global slash command: {:#?}", guild_command);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN missing in .env");
+
+    let mut client = Client::builder(token, GatewayIntents::empty())
+            .event_handler(Handler)
+            .await
+            .expect("Failed to create client");
+    
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
